a65ffa8fdc1f5d39c3c740d7ad34fcf46e1ea214
koojina <70617718+koojina@users.noreply.github.com>
2022-08-19 22:41:44 +0900

    outline

---
+++ Assets/OutlineEffect/Samples/Demo/Toggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cakeslice
+{
+    public class Toggle : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //아웃라인 키는 부분
+            if(Input.GetKeyDown(KeyCode.K))
+            {
+                GetComponent<Outline>().enabled = !GetComponent<Outline>().enabled;
+            }
+        }
+    }
+}
